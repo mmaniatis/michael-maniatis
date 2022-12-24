4aa27e58c7504d3d29de8e6e33ac258757c13290
Michael Maniatis <mikejohnmaniatis@gmail.com>
2022-12-24 11:30:42 -0600

    Navbar test file

---
+++ src/Components/Navbar/Navbar.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+import Navbar from './Navbar';
+import { BrowserRouter as Router} from "react-router-dom";
+
+test('renders ', () => {
+    <Router>
+        render(<Navbar />);
+    </Router>
+});
