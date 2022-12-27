0fa123f6f5970a17dc1b8b8eb67e81f2284b9d3a
Michael Maniatis <mikejohnmaniatis@gmail.com>
2022-12-27 15:44:12 -0600

    added new icons

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import './LandingPage.css'
-import buildingSoftware from '../photos/building-software.png'
+import './LandingPage.css';
+import mainLogo from '../photos/software-2.png';
+import codingLogo from '../photos/coding.png';
+import softwareLogo from '../photos/web-development.png';
+import Contact from './Contact';
 
 class LandingPage extends Component {
     render() {
@@ -8,12 +11,49 @@ class LandingPage extends Component {
         return <div className="landing-container">
             <div className="landing-top">
                 <span className="text">Helping Local Businesses Build <br /> Their Software</span>  
+                <img className="landing-image" src={mainLogo} height="200px" width="400px"/>
             </div>
 
-            <div className="landing-top-photo">
-                <img src={buildingSoftware} height="400px" width="600px"/>
+
+            
+            <div className="landing-middle-expertise">
+                <div className="landing-middle-expertise-item">
+                    <img className="site-image" src={codingLogo} height="150px" width="200px"/>
+                    <span className="expertise-header">Software Consultation</span>
+                </div>
+
+                <div className="landing-middle-expertise-item">
+                    <img src={codingLogo} height="150px" width="200px"/>
+                    <span className="expertise-header">Client First Support</span>
+                </div>
+
+                <div className="landing-middle-expertise-item">
+
+                    <img src={codingLogo} height="150px" width="200px"/>
+                    <span className="expertise-header">Communication</span>
+                </div>
+
+                <div className="landing-middle-expertise-item">
+
+                    <img src={codingLogo} height="150px" width="200px"/>
+                    <span className="expertise-header">Resolving bottlenecks</span>
+                </div>
+
+                <div className="landing-middle-expertise-item">
+
+                    <img src={codingLogo} height="150px" width="200px"/>
+                    <span className="expertise-header">Technology Support</span>
+                </div>
             </div>
 
+            <div className="landing-contact">
+                <Contact />
+            </div>
+
+            <div className="landing-footer">
+                <a href="https://www.flaticon.com/free-icons/development" title="development icons">Development icons created by Freepik - Flaticon</a>
+
+            </div>
         </div>;
     }
 }
